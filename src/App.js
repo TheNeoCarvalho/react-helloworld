82d00b6e1f477e6e59468fd5f43b3e323f1e737a
Manoel Carvalho <manoelmecat@gmail.com>
2019-04-26 07:41:54 -0300

    acessando state

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import "./App.css";
 //stateful components
 class App extends Component {
   state = {
-    nome: "manoel",
-    media: 5.8,
     pessoas: [
       {
         id: 1,
         nome: "Manolo",
         email: "manolo@gmail.com",
-        img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR611OngLX1ZHGdb6_ozHHKyygJLTUYyd0stvkSKp2KbuxBU0AG"
+        img: "user.png"
       },
       {
         id: 2,
         nome: "Maria",
         email: "maria@gmail.com",
+        img: "user.png"
+      },
+      {
+        id: 3,
+        nome: "Valéria",
+        email: "valeria@gmail.com",
         img: "user.png"
       }
     ]
@@ -26,23 +29,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {}
-        <BoxUser img="user.png" nome="Manoel" email="manoel@gmail.com" />
-        <BoxUser img="user.png" nome="Maria" email="maria@gmail.com" />
-        <BoxUser img="user.png" nome="Jose" email="jose@gmail.com" />
-        <BoxUser img="user.png" nome="Joao" email="joao@gmail.com" />
+        {this.state.pessoas.map(pessoa => {
+          return (
+            <BoxUser img={pessoa.img} nome={pessoa.nome} email={pessoa.email} />
+          );
+        })}
       </div>
     );
   }
 }
-
-//stateless components
-// const App = () => {
-//   return (
-//     <div className="App">
-//       <h1>Neo</h1>
-//     </div>
-//   );
-// };
 
 export default App;
